57becbcf07aeb9a7135df400e6de8c9480fa4172
Oishik <oishikmandal@gmail.com>
2020-07-03 14:22:56 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# easy-trans-web
+
+Web page for [EasyTrans](https://github.com/khuzema786/EasyTrans-QR_MAP) app
---
+++ scripts/app.js
@@ -0,0 +1,33 @@
+// Hello World Testing
+
+console.log('YOOOOOOOO.');
+
+
+// DOM Elements
+
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+const body = document.body;
+
+
+// Apply the cached theme on reload
+
+const theme = localStorage.getItem('theme');
+const isSolar = localStorage.getItem('isSolar');
+
+if (theme) {
+    body.classList.add(theme);
+    isSolar && body.classList.add('solar');
+}
+
+// Button Event Handlers for theme setting
+
+darkButton.onclick = () => {
+    body.classList.replace('light', 'dark');
+    localStorage.setItem('theme', 'dark');
+};
+
+lightButton.onclick = () => {
+    body.classList.replace('dark', 'light');
+    localStorage.setItem('theme', 'light');
+};
